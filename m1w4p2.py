78d457bdafd7a1e264bea617c6416d1add4d9063
GSP-YT <128293212+GSP-YT@users.noreply.github.com>
2024-03-02 11:18:48 +0530

    Add files via upload

--- m1w4p2.py
+++ m1w4p2.py
@@ -1,5 +1,6 @@
-userInput = list((input("Enter num list :").split()))
+userInput = list(input("Enter num list :").split())
 numList = []
-for num in range(userInput): numList.append(int(num))
-def isEvent(x) : return x % 2 == 0
-print(filter(isEvent,numList))
+for num in userInput: numList.append(int(num))
+def isEvent(x): return x % 2 == 0
+numIterator = filter(isEvent,numList)
+print(list(numIterator))
